f0773bfd516d75a7482875f433230e349671fe86
Kevin <kevin.mavier@hotmail.com>
2024-08-28 15:16:49 +0200

    set winner function

--- component/Board.js
+++ component/Board.js
@@ -7,6 +7,27 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
 
+    function calculateWinner(squares) {
+
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 
     function handleClick(i) {
         const newSquares = squares.slice();
@@ -19,6 +40,18 @@ export default function Board() {
         setXIsNext(!xIsNext);
         console.log(newSquares)
     }
+    const winner = calculateWinner(squares);
+
+    let status;
+
+    if (winner) {
+        status = 'Gagnant est : ' + winner;
+    } else if (!squares.includes(null)) {
+        status = 'Match Nul';
+    } else {
+        status = 'Prochain joueur : ' + (xIsNext ? 'X' : '0');
+    }
+
 
     function renderSquare(i) {
         return <Square squares={i} onClick={() => handleClick(i)} />
@@ -26,6 +59,7 @@ export default function Board() {
 
     return (
         <div>
+            <div className={styles.status}>{status}</div>
             <div className={styles.boardRow}>
                 {renderSquare(0)}
                 {renderSquare(1)}
